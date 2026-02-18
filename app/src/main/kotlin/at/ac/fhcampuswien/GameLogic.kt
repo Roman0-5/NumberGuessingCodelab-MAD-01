8e6db87927e94e8e1dc04fe9b4ca59446d3eca62
Roman0-5 <chowerroman@gmail.com>
2026-02-18 13:08:06 +0100

    Task 1 Done

--- app/src/main/kotlin/at/ac/fhcampuswien/GameLogic.kt
+++ app/src/main/kotlin/at/ac/fhcampuswien/GameLogic.kt
@@ -2,6 +2,8 @@ package at.ac.fhcampuswien
 
 import at.ac.fhcampuswien.GuessResult
 
-class GameLogic() {
-
+class GameLogic(initialDigitsToGuess: Int) {
+    // Kotlin has getters and setters on public by default
+    var digitsToGuess = initialDigitsToGuess
+        private set // declare private setter
 }
